f0dd07c888b1b18a53317f54ee1a1d20c35af843
IvetMartinez <ivimar637@gmail.com>
2026-06-09 18:44:12 -0600

    Practica No.6: Estructura y Componentes

--- Practica 5/miapp2/App.js
+++ Practica 5/miapp2/App.js
@@ -1,15 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+/* Zona 1 : Importaciones de componentes y archivos */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Saludo} from './componentes/Saludo'
+import {Saludo2} from './componentes/Saludo2'
+
+
+/* Zona 2 : Main - Hogar de los componentes  */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola mundo RN!</Text>
+      <Text>---------------------Componente simple ----------------------</Text>
+
+      <Saludo></Saludo>
+
+      <Text>-------------------Componente compuesto ---------------------</Text>
+
+      <Saludo2></Saludo2>
+      
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+/* Zona 3 : Estilos y posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
