ea58971529ff9d6c20a8d74127873df71049c4a5
IvetMartinez <ivimar637@gmail.com>
2026-06-28 19:56:46 -0600

    Práctica 13: Repaso de Componentes I

---
+++ Repaso/repaso1/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegistroEventoScreen from './screens/RegistroDeEventoUniversitario';
+
+const App = () => {
+  return <RegistroEventoScreen />;
+};
+
+export default App;
